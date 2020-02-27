6a77360d53f8f303abce73f4c55395412ca32324
Noah <noah@Noahs-MacBook-Pro.local>
2020-02-27 12:37:16 -0500

    quadruple jump

--- GitTest/Assets/Movement.cs
+++ GitTest/Assets/Movement.cs
@@ -8,8 +8,10 @@ public class Movement : MonoBehaviour
     private Rigidbody myRigidbody;
 	public float jump;
 	private bool canJump2;
-    // Start is called before the first frame update
-    void Start()
+	private bool canJump3;
+	private bool canJump4;
+	// Start is called before the first frame update
+	void Start()
     {
         myRigidbody = GetComponent<Rigidbody>();
     }
@@ -18,12 +20,30 @@ public class Movement : MonoBehaviour
     void Update()
     {
         myRigidbody.velocity = new Vector3(speed * Input.GetAxis("Horizontal"), myRigidbody.velocity.y, speed * Input.GetAxis("Vertical"));
+		if (canJump4)
+		{
+			if (Input.GetKeyDown("space"))
+			{
+				myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jump, myRigidbody.velocity.z);
+				canJump4 = false;
+			}
+		}
+		if (canJump3)
+		{
+			if (Input.GetKeyDown("space"))
+			{
+				myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jump, myRigidbody.velocity.z);
+				canJump3 = false;
+				canJump4 = true;
+			}
+		}
 		if (canJump2)
 		{
 			if (Input.GetKeyDown("space"))
 			{
 				myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jump, myRigidbody.velocity.z);
 				canJump2 = false;
+				canJump3 = true;
 			}
 		}
 		if (myRigidbody.velocity.y == 0f)
